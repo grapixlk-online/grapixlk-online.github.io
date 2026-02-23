65621e83c35c7ea93a25043f5b3b03eb1c61ad03
grapixlk-online <grapixlk@gmail.com>
2026-02-23 23:12:19 +0530

    Create firebase.js

---
+++ assets/js/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+// ✅ PASTE your config here
+export const firebaseConfig = {
+  apiKey: "PASTE",
+  authDomain: "PASTE",
+  projectId: "PASTE",
+  storageBucket: "PASTE",
+  messagingSenderId: "PASTE",
+  appId: "PASTE"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
