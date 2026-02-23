fdd667d81c4e1f73097a3ef0789e09804bf13889
grapixlk-online <grapixlk@gmail.com>
2026-02-23 23:16:03 +0530

    Update firebase.js

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,14 +1,14 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
-// ✅ PASTE your config here
-export const firebaseConfig = {
-  apiKey: "PASTE",
-  authDomain: "PASTE",
-  projectId: "PASTE",
-  storageBucket: "PASTE",
-  messagingSenderId: "PASTE",
-  appId: "PASTE"
+const firebaseConfig = {
+  apiKey: "AIzaSyBE_5NvG7cYnw_cssw7YfvVxEHtp4M-Ajc",
+  authDomain: "grapixlk-online.firebaseapp.com",
+  projectId: "grapixlk-online",
+  storageBucket: "grapixlk-online.firebasestorage.app",
+  messagingSenderId: "18083338347",
+  appId: "1:18083338347:web:258b32ee4fe9c4d2bef92e",
+  measurementId: "G-YS4KLHLBL2"
 };
 
 const app = initializeApp(firebaseConfig);
